a8433cb88b50a6aa1aa1d7e355a4d6bf0f6be0cc
Yann Baumgartner <ybaumgartner@gmail.com>
2016-02-26 18:27:28 +0100

    Change settings for search

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Yann Baumgartner'
 SITENAME = u'Histoires de briques'
 SITESUBTITLE = 'Un site dédié aux LEGO®'
-SITEURL = ''
+SITEURL = 'https://github.com/yannbaumgartner/histoires-de-briques'
 GITHUB_URL = 'https://github.com/yannbaumgartner/histoires-de-briques.git'
 TIMEZONE = 'Europe/Zurich'
 DEFAULT_LANG = u'fr'
@@ -33,6 +33,7 @@ THEME = 'themes/pelican-bootstrap3'
 BOOTSTRAP_THEME = 'flatly'
 
 DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives', 'search'))
+SEARCH_URL = '/search.html'
 
 DISPLAY_ARTICLE_INFO_ON_INDEX = True
 DISPLAY_TAGS_ON_SIDEBAR = 'True'
